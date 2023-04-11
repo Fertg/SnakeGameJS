17caca713c988fdabef43dbaaac4d4a65e982440
Fertg92 <fertg92@gmail.com>
2023-04-11 11:40:24 +0200

    add alimento y crecimiento

--- snake.js
+++ snake.js
@@ -6,9 +6,9 @@ const head={
     x:0,
     y:0
 };
+const body=[];
 
 let food=null;
-
 
 let dx = 0;
 let dy = 0;
@@ -16,7 +16,7 @@ let dy = 0;
 
 
 
-setInterval(main,1000);
+setInterval(main,500); //se llama 1 vez por segundo
 
 
 
@@ -27,25 +27,47 @@ function main(){
 
 
 function update(){
+
+
+    let prevX,prevY;
+    //salvar la posicion del ultimo posicion de la serpietne
+    if(body.length>=1){
+     prevX = body[body.length-1].x;
+     prevY = body[body.length-1].y;
+    }else{
+    prevX=head.x;
+    prevY=head.y;
+    }
+    //cuerpo sigue a la cabeza
+    for (let i=body.length-1; i>=1 ;--i){
+        body[i].x = body[i-1].x
+        body[i].y = body[i-1].y
+
+    }
+    if(body.length>=1){
+    body[0].x=head.x;
+    body[0].y=head.y;}
     //actualiza cabeza de la serpiente
     head.x+=dx;
     head.y+=dy;
     //detecta si la serpiente ha consumido comida
     if(food && head.x===food.x && head.y===food.y){
         food=null;
+        increaseSnakeSize(prevX,prevY);
     }
     //crea comida si no hay
     if(!food){       
-        food = {x:getRandomX(),y:getRandomY()};
-        console.log('Mover hacia arriba'+food.x + food.y);
+        food = {x:getRandomX(),y:getRandomY()};      
     }
 };
 
 function draw(){
-    context.fillStyle='black';
+    context.fillStyle='black'; //Bkgrd 
     context.fillRect(0,0,myCanvas.width,myCanvas.height);
-    drawObjet(head,'lime');
-    drawObjet(food,'red');
+    
+    drawObjet(head,'lime');//draw cabeza
+    body.forEach(elem =>drawObjet(elem,'lime'));
+    drawObjet(food,'red');//draw comida
 }
 
 function getRandomX(){
@@ -59,6 +81,15 @@ function drawObjet(obj,color){
     context.fillStyle=color;
     context.fillRect(obj.x,obj.y,SIZE,SIZE);
 }
+
+
+function increaseSnakeSize(prevX,prevy){
+ body.push({
+    x:prevX , y:prevy }
+    );
+}
+
+
 
 document.addEventListener('keydown',moverSnake);
 
